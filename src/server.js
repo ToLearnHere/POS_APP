9f61f25ebf1aa3fa89383745d6f10055c7c73aee
Marvz <marvinloiegonzales18@gmail.com>
2025-11-20 06:00:35 +0800

    Product Route Api added at server js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { initDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
-
+import productRoutes from './routes/productRoute.js';
 import transactionsRoute from "./routes/transactionsRoute.js"
 import job from "./config/cron.js";
 
@@ -24,6 +24,10 @@ app.get("/api/health", (req, res) => {
 }) 
 
 app.use("/api/transactions", transactionsRoute);
+
+
+// ... after app = express(), cors, etc.
+app.use('/api', productRoutes);   // THIS IS THE MISSING LINE
 
 console.log("my port:",process.env.PORT)
 
