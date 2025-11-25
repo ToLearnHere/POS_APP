adf1b9882e4af6206b6282fcd2b4c2b93348dfa3
Marvz <marvinloiegonzales18@gmail.com>
2025-11-25 08:14:36 +0800

    delete product route, controller, hook

--- src/config/upstash.js
+++ src/config/upstash.js
@@ -1,12 +1,11 @@
-import { Redis } from '@upstash/redis';
-import {Ratelimit} from "@upstash/ratelimit";
-//npm i @upstash/redis@1.34.9 @upstash/ratelimit@2.0.5 
+import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit";
+
 import "dotenv/config";
 
-const ratelimiter = new Ratelimit({
-  redis: Redis.fromEnv(), 
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
   limiter: Ratelimit.slidingWindow(100, "60 s"),
 });
 
-export default ratelimiter
-
+export default ratelimit;
